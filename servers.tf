699e1ad36a91c758b48ed82e630ba24ccd0d6773
ameen <ameenmustafa2107@gmail.com>
2024-12-04 16:44:11 +0530

    for-each instnce creation

--- servers.tf
+++ servers.tf
@@ -3,20 +3,73 @@ data "aws_security_group" "allow-all" {
   name = "allow-all"
 }
 
-variable "instance_type"{
-    default = "t2.micro"
-    }
+# variable "instance_type"{
+#     default = "t2.micro"
+#     }
+
+# variable "components"{
+#     default = ["frontend","mongodb","catalogue"]
+#     }
 
 variable "components"{
-    default = ["frontend","mongodb","catalogue"]
+    default = {
+        frontend={
+            name = "frontend"
+            instance_type = "t2.micro"
+            }
+
+         mongodb={
+                    name = "mongodb"
+                    instance_type = "t3.micro"
+                    }
+          catalogue={
+                     name = "catalogue"
+                     instance_type = "t3.micro"
+                     }
+
+           redis={
+                      name = "redis"
+                      instance_type = "t3.micro"
+                      }
+            user={
+                       name = "user"
+                       instance_type = "t3.micro"
+                       }
+             cart={
+                        name = "cart"
+                        instance_type = "t3.micro"
+                        }
+              mysql={
+                         name = "mysql"
+                         instance_type = "t3.micro"
+                         }
+               shipping={
+                          name = "shipping"
+                          instance_type = "t3.micro"
+                          }
+                rabbitmq={
+                           name = "rabbitmq"
+                           instance_type = "t3.micro"
+                           }
+                payment={
+                                           name = "payment"
+                                           instance_type = "t3.micro"
+                                           }
+                dispatch={
+                                                           name = "dispatch"
+                                                           instance_type = "t3.micro"
+                                                           }
     }
-resource "aws_instance" "frontend" {
-    count = length(var.components)
+}
+resource "aws_instance" "instance" {
+    for_each = var.components
+#     count = length(var.components)
   ami           = "ami-0453ec754f44f9a4a"
-  instance_type = var.instance_type
+#   instance_type = var.instance_type
+  instance_type = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
   tags = {
-    Name = var.components[count.index]
+    Name = each.value["name"]
   }
 }
 
